bbfcae19acb191c870185c1a8de16fea3973cc46
aoirint <aoirint@gmail.com>
2022-05-01 03:59:18 +0900

    asc download & stop paging if new found

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from pixivpy3 import *
 import time
+import tempfile
 
 load_dotenv()
 
@@ -14,31 +15,39 @@ api = AppPixivAPI()
 
 api.auth(refresh_token=REFRESH_TOKEN)
 
+illust_root_dir = Path(ROOT_DIR)
+downloaded_user_ids = set([path.name for path in illust_root_dir.iterdir()])
+downloaded_user_illust_ids = set([(user_id, path.name) for user_id in downloaded_user_ids for path in Path(illust_root_dir, user_id).iterdir()])
+downloaded_illust_ids = set([illust_id for user_id, illust_id in downloaded_user_illust_ids])
+
 result = api.user_bookmarks_illust(user_id=USER_ID, req_auth=True)
 
+new_illusts_desc = []
 while True:
     illusts = result.illusts
+    page_new_illusts_desc = []
 
     for illust in illusts:
-        pages = illust.meta_pages 
         user = illust.user
 
-        illust_dir = Path(ROOT_DIR, str(user.id), str(illust.id))
-        illust_dir.mkdir(exist_ok=True, parents=True)
+        illust_dir = Path(illust_root_dir, str(user.id), str(illust.id))
+        if illust_dir.exists():
+            # Detect difference addition & download continuously
+            num_local_pages = len(list(illust_dir.iterdir()))
+            num_remote_pages = 1 if illust.meta_single_page else len(illust.meta_pages)
 
-        print(user.id, user.name)
-        print(illust.id, illust.title)
-        if illust.meta_single_page:
-            image_url = illust.meta_single_page.original_image_url
-            print(image_url)
-            if api.download(image_url, path=illust_dir):
-                time.sleep(1)
-        else:
-            for page in pages:
-                image_url = page.image_urls.original
-                print(image_url)
-                if api.download(image_url, path=illust_dir):
-                    time.sleep(1)
+            if num_local_pages == num_remote_pages:
+                continue
+
+        page_new_illusts_desc.append(illust)
+
+    # if no new illust in newest page, stop paging (asc download)
+    if len(page_new_illusts_desc) == 0:
+        print('No new illust found in page')
+        break
+
+    new_illusts_desc.extend(page_new_illusts_desc)
+    print(f'Paging (found: {len(new_illusts_desc)})')
 
     next_qs = api.parse_qs(result.next_url)
     if not next_qs:
@@ -47,3 +56,26 @@ while True:
     time.sleep(1)
     result = api.user_bookmarks_illust(**next_qs)
 
+print(f'New Illusts: {len(new_illusts_desc)}')
+
+# download new illust in asc order
+new_illusts_asc = reversed(new_illusts_desc)
+for illust in new_illusts_asc:
+    user = illust.user
+
+    illust_dir = Path(illust_root_dir, str(user.id), str(illust.id))
+    illust_dir.mkdir(exist_ok=True, parents=True)
+
+    print(user.id, user.name, illust.id, illust.title)
+    if illust.meta_single_page:
+        image_url = illust.meta_single_page.original_image_url
+        print(image_url)
+        if api.download(image_url, path=illust_dir):
+            time.sleep(1)
+    else:
+        pages = illust.meta_pages 
+        for page in pages:
+            image_url = page.image_urls.original
+            print(image_url)
+            if api.download(image_url, path=illust_dir):
+                time.sleep(1)
